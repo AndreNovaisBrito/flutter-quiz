711b615213f294c50ceb3aac83fb6b04e93847c7
AndreNovaisBrito <novaisandre.dev@github.com>
2024-05-19 03:53:21 -0300

    start_screen

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:advanced_basics/start_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 87, 36, 175),
+                Color.fromARGB(255, 65, 24, 138),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
